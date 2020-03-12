a25227f60faa55769b65561a35177554db1921da
Nandish J U <nandish.ju@digicollect.com>
2020-03-12 12:16:30 +0530

    added paypal and checkout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+// import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Default from "./components/Default";
+import Cart from "./components/Cart/Cart";
+import Details from "./components/Details";
+import Modal from "./components/Modal";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navbar></Navbar>
+      <Switch>
+        <Route path="/details" component={Details}></Route>
+        <Route path="/cart" component={Cart}></Route>
+        <Route path="/" component={ProductList}></Route>
+        <Route component={Default}></Route>
+      </Switch>
+      <Modal></Modal>
+    </React.Fragment>
   );
 }
 
